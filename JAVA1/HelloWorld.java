5be859b9be7905a495c72e0578a5fd68a4d48cf0
Sting Yellow <nsong568@gmail.com>
2024-04-12 16:51:01 +0700

    add file

---
+++ JAVA1/HelloWorld.java
@@ -0,0 +1,11 @@
+package JAVA1;
+
+/**
+ * HelloWorld
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World lan thu 100 !");
+    }
+}
